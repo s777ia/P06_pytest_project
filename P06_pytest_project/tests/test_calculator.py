0ab44616a30e3c1f0c90b99e6a910395d6d7a37b
s777ia <23046128@myrp.edu.sg>
2024-05-24 14:16:55 +0800

    initial commit

---
+++ P06_pytest_project/tests/test_calculator.py
@@ -0,0 +1,51 @@
+from Calculator.calculator import Calculator
+
+class TestCalculator:
+    def test_add(self):
+        # arrange
+        a = 4321
+        b = 1234
+        cal = Calculator()
+
+        # act
+        result = cal.add(a, b)
+
+        # assert
+        expected = 5555
+        assert result == expected
+    def test_subtract(self):
+        # arrange
+        a = 43
+        b = 12
+        cal = Calculator()
+
+        # act
+        result = cal.subtract(a, b)
+
+        # assert
+        expected = 31
+        assert result == expected
+    def test_multiply(self):
+        # arrange
+        a = 4
+        b = 1
+        cal = Calculator()
+
+        # act
+        result = cal.multiply(a, b)
+
+        # assert
+        expected = 4
+        assert result == expected
+    def test_divide(self):
+        # arrange
+        a = 4
+        b = 2
+        cal = Calculator()
+
+        # act
+        result = cal.divide(a, b)
+
+        # assert
+        expected = 2.0
+        assert result == expected
